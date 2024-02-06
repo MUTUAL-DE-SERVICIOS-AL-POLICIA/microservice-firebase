e9f283b7ce4460edd002de00a057e986a419ed8d
MaxRamirez <leorexx100@gmail.com>
2024-02-06 16:17:17 -0400

    Ruta de impresión

--- src/controllers/printer.controller.js
+++ src/controllers/printer.controller.js
@@ -1,42 +1,88 @@
 const { request, response } = require("express");
 const pdfToPrinter = require('pdf-to-printer');
+const path = require('path');
 
+const fs = require('fs');
+// const launchPrinter = async (req = request, res = response) => {
+//     try {
+//         const pdfUrl = req.body.path;
+//         let pages = req.body.pages;
+//         console.log("all printers: ", pdfToPrinter.getPrinters().then(console.log))
+
+//         const defaultPrinter = await pdfToPrinter.getDefaultPrinter()
+//         console.log("default printer: ", defaultPrinter)
+
+//         if(Object.keys(defaultPrinter).length != 0) {
+//             if(pages == null || pages == undefined) pages = 1
+//             const options = {
+//                 printer: defaultPrinter.name,
+//                 pages: pages,
+//                 paperSize: "Ejecutivo",
+//                 scale: "fit"
+//             }
+//             await pdfToPrinter.print(pdfUrl, options)
+//             res.json({
+//                 message: "Successfull print",
+//                 data: [ options ]
+//             })
+//         } else res.status(400).json({
+//             message: "No printer available",
+//             data: []
+//         })
+//     } catch(error) {
+//         console.log(error)
+//         res.status(500).json({
+//             message: "An error ocurred",
+//             data: []
+//         })
+//     }
+// }
 
 const launchPrinter = async (req = request, res = response) => {
     try {
-        const pdfUrl = req.body.path;
-        let pages = req.body.pages;
-        console.log("all printers: ", pdfToPrinter.getPrinters().then(console.log))
+        console.log(req.files);
+        const _path = req.files[0].path;
+        // console.log("PDF almacenado temporalmente");
+        const filePathAbsolute = path.resolve('', _path);
+        console.log(filePathAbsolute);
 
-        const defaultPrinter = await pdfToPrinter.getDefaultPrinter()
-        console.log("default printer: ", defaultPrinter)
+        const defaultPrinter = await pdfToPrinter.getDefaultPrinter();
+        console.log("Impresora predeterminada: ", defaultPrinter);
 
-        if(Object.keys(defaultPrinter).length != 0) {
-            if(pages == null || pages == undefined) pages = 1
+        let pages = 1;
+        if (Object.keys(defaultPrinter).length !== 0) {
+            if (pages == null || pages == undefined) pages = 1;
             const options = {
                 printer: defaultPrinter.name,
                 pages: pages,
                 paperSize: "Ejecutivo",
                 scale: "fit"
-            }
-            await pdfToPrinter.print(pdfUrl, options)
+            };
+
+            // await pdfToPrinter.print(pdfFileName, options);
+            await pdfToPrinter.print(filePathAbsolute, options);
+            
+            // fs.unlinkSync(pdfFileName); // Eliminar el archivo temporal después de la impresión
+
             res.json({
-                message: "Successfull print",
-                data: [ options ]
-            })
-        } else res.status(400).json({
-            message: "No printer available",
-            data: []
-        })
-    } catch(error) {
-        console.log(error)
+                message: "Impresión exitosa",
+                data: [options]
+            });
+        } else {
+            res.status(400).json({
+                message: "No hay impresora disponible",
+                data: []
+            });
+        }
+    } catch (error) {
+        console.log(error);
         res.status(500).json({
-            message: "An error ocurred",
+            message: "Ocurrió un error",
             data: []
-        })
+        });
     }
-}
+};
 
 module.exports = {
-    launchPrinter
+    launchPrinter,
 };
